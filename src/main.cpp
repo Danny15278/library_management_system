f62743a08b5f4af47b7ed26be6a0f3d1bb66de10
Danny15278 <danny26574@gmail.com>
2025-06-03 02:48:17 +1200

    Borrowing interaction added

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,29 @@ int main() {
         std::cout << "3. List all Books\n";
         std::cout << "4. Quit\n";
         
-        std::cout << "\n Please enter your choice: ";
+        std::cout << "\n Please enter your choice: "; 
         int choice;
         std::cin >> choice;
-        if (std::cin.fail()) {
+
+        if (std::cin.fail()) { // Catch invalid inputs
             std::cout << "Invalid input. Please try again!\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         }
 
-        if (choice < 1 || choice > 4) {
+        if (choice < 1 || choice > 4) { // Catch values outside of range
             std::cout << "Please enter a value between 1 and 4.";
             continue;
+        }
+
+
+        if (choice == 1) { // Input 1: Borrowing a book from the LMS
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::string title;
+            std::cout << "Please enter the title: ";
+            std::getline(std::cin, title);
+            myLibrary.borrowBook(title);
         }
     }
 }
